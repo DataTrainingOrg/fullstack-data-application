da371f80c27f98505b8bc86548fdde8ef744ec59
Juliette <juliette.rochon@edu.esiee.fr>
2021-11-16 17:14:43 +0100

    Formulaire : en cours

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,44 @@
-# Import the required libraries
-from flask import Flask
+from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_marshmallow import Marshmallow
-from flask_cors import CORS
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SECRET_KEY'] = "random string"
 
-# Create various application instances
-# Order matters: Initialize SQLAlchemy before Marshmallow
-db = SQLAlchemy()
-migrate = Migrate()
-ma = Marshmallow()
-cors = CORS()
+db = SQLAlchemy(app)
 
+class students(db.Model):
+   id = db.Column('student_id', db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+   city = db.Column(db.String(50))
+   addr = db.Column(db.String(200)) 
+   pin = db.Column(db.String(10))
 
-def create_app():
-    """Application-factory pattern"""
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def __init__(self, name, city, addr,pin):
+   self.name = name
+   self.city = city
+   self.addr = addr
+   self.pin = pin
 
-    # Initialize extensions
-    # To use the application instances above, instantiate with an application:
-    db.init_app(app)
-    migrate.init_app(app, db)
-    ma.init_app(app)
-    cors.init_app(app)
+@app.route('/')
+def show_all():
+   return render_template('show_all.html', students = students.query.all() )
 
-    return app
+@app.route('/new', methods = ['GET', 'POST'])
+def new():
+   if request.method == 'POST':
+      if not request.form['name'] or not request.form['city'] or not request.form['addr']:
+         flash('Please enter all the fields', 'error')
+      else:
+         student = students(request.form['name'], request.form['city'],
+            request.form['addr'], request.form['pin'])
+         
+         db.session.add(student)
+         db.session.commit()
+         flash('Record was successfully added')
+         return redirect(url_for('show_all'))
+   return render_template('new.html')
+
+if __name__ == '__main__':
+   db.create_all()
+   app.run(debug = True)
