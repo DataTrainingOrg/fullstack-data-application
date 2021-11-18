5cdc9d5990d039d96d1e2afc34fe76859a6d92d3
Juliette <juliette.rochon@edu.esiee.fr>
2021-11-18 15:21:36 +0100

    Deleted part on routes.py that are not useful anymore

--- backend/routes.py
+++ backend/routes.py
@@ -24,35 +24,6 @@ def articles():
 	return jsonify(results)
 
 
-@app.route("/signin", methods=["GET"], strict_slashes=False)
-def signin():
-	if request.method == 'POST':
-		connection = pymongo.MongoClient("mongodb://mongodb:27017/mongodb")
-		db_m = connection["mongodb"]
-		collection = db_m['User']
-		form_username = str(request.form.get('username'))
-		form_password = str(request.form.get('password'))
-
-		try:
-			mongo_pwd = collection.find({'username': form_username})[0]['password']
-		except:
-			mongo_pwd=''
-
-		try:
-			if mongo_pwd == form_password:
-				server.config["LOGGED"] = True
-				server.config["USERNAME"] = form_username
-				return redirect('/')
-			else:
-				flash('Wrong password')
-				return redirect('/signin')
-			
-		except:
-			
-			return redirect('/signupko')
-
-	return render_template('signin.html')
-
 
 if __name__ == "__main__":
 	app.run(debug=True)
