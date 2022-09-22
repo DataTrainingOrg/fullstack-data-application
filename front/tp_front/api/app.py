565d811d403f435366d5022ba84c95f02fc3b1e7
nico <nicolasvo95@gmail.com>
2022-09-23 01:31:01 +0200

    add cors and post

--- front/tp_front/api/app.py
+++ front/tp_front/api/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello from api!'
+    return jsonify({"title": "Hello from api!"})
 
+
+@app.route("/job", methods=["POST"])
+def print_job():
+    title = request.json["title"]
+    company = request.json["company"]
+    salary = request.json["salary"]
+    result = {"message": f"{title}s at {company} earn {salary} dollars"}
+    return jsonify(result)
